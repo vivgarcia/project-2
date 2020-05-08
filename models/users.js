63595a1993936f13d2b55faef26074076fa8010d
AshfoMon <ashfordmonte@gmail.com>
2020-05-08 06:34:11 -0500

    Added 3 database models to store app information, updated MySQL packages

---
+++ models/users.js
@@ -0,0 +1,35 @@
+// This model will hold all user data and will only allow unique usernames and passwords
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+        notEmpty: true
+      }
+    },
+    platform: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  return User;
+};
