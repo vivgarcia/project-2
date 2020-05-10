a9fc09678b547b1186f0132d258860a3b1879a20
Lyyte <lyytefaridi@gmail.com>
2020-05-10 11:55:16 -0500

    added method overide module to allow logout route, added register button in signin page, reformatted sign button, added chckedSignedIn and checkNotSignedIn functions to ensure user is signed in before accessing dashboard, and user can not access regiser to sign pages if signed in, added logout route

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,99 @@
-$(document).ready(() => {
+// Get references to page elements
+var $exampleText = $("#example-text");
+var $exampleDescription = $("#example-description");
+var $submitBtn = $("#submit");
+var $exampleList = $("#example-list");
 
+// The API object contains methods for each kind of request we'll make
+var API = {
+  saveExample: function(example) {
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "api/examples",
+      data: JSON.stringify(example)
+    });
+  },
+  getExamples: function() {
+    return $.ajax({
+      url: "api/examples",
+      type: "GET"
+    });
+  },
+  deleteExample: function(id) {
+    return $.ajax({
+      url: "api/examples/" + id,
+      type: "DELETE"
+    });
+  }
+};
 
-    function createProfile(username) {
-        User.findAll({
-            where: {
-                username: username
-            }
-        }).then((res) => {
-            let name = res.name;
-            let email = res.email;
-            let platform = res.platform;
+// refreshExamples gets new examples from the db and repopulates the list
+var refreshExamples = function() {
+  API.getExamples().then(function(data) {
+    var $examples = data.map(function(example) {
+      var $a = $("<a>")
+        .text(example.text)
+        .attr("href", "/example/" + example.id);
 
-
-
-            $('#userName').text("Name: " + name)
-            $('#itememail').text("email: " + email);
-            $('#itemPlatform').text("Platform: " + platform)
-
+      var $li = $("<li>")
+        .attr({
+          class: "list-group-item",
+          "data-id": example.id
         })
-    };
+        .append($a);
 
-    let addNewGame = () => {
-        event.preventDefault();
+      var $button = $("<button>")
+        .addClass("btn btn-danger float-right delete")
+        .text("ｘ");
 
-        let newGame = $('#newGame').val();
-        console.log(newGame)
-        addGame(newGame)
+      $li.append($button);
 
-    };
+      return $li;
+    });
 
-    $('#addnewGame').on('click', addNewGame);
+    $exampleList.empty();
+    $exampleList.append($examples);
+  });
+};
 
-    let addGame = (gameData) => {
-        $.post('/add-game', gameData)
-    }
+// handleFormSubmit is called whenever we submit a new example
+// Save the new example to the db and refresh the list
+var handleFormSubmit = function(event) {
+  event.preventDefault();
 
-});
+  var example = {
+    text: $exampleText.val().trim(),
+    description: $exampleDescription.val().trim()
+  };
+
+  if (!(example.text && example.description)) {
+    alert("You must enter an example text and description!");
+    return;
+  }
+
+  API.saveExample(example).then(function() {
+    refreshExamples();
+  });
+
+  $exampleText.val("");
+  $exampleDescription.val("");
+};
+
+// handleDeleteBtnClick is called when an example's delete button is clicked
+// Remove the example from the db and refresh the list
+var handleDeleteBtnClick = function() {
+  var idToDelete = $(this)
+    .parent()
+    .attr("data-id");
+
+  API.deleteExample(idToDelete).then(function() {
+    refreshExamples();
+  });
+};
+
+// Add event listeners to the submit and delete buttons
+$submitBtn.on("click", handleFormSubmit);
+$exampleList.on("click", ".delete", handleDeleteBtnClick);
