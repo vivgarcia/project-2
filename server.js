9c714d45a3a866289d16d9b06dc753ddb850ad30
AshfoMon <ashfordmonte@gmail.com>
2020-05-10 15:22:27 -0500

    Revert "Revert "added method overide module to allow logout route, added register button in signin page, reformatted sign button, added chckedSignedIn and checkNotSignedIn functions to ensure user is signed in before accessing dashboard, and user can not access regiser to sign pages if signed in, added logout route""
    This reverts commit b0e464ae19c3fc738ad599834817f29717a65b1e.
    
    	modified:   config/config.json
    	new file:   config/passport-config.js
    modified:   models/users.js
    	modified:   package-lock.json
    	modified:   package.json
    	deleted:    public/images/addNewGame.png
    	deleted:    public/images/createAccount.png
    	deleted:    public/images/deadpool.jpg
    	deleted:    public/images/gameButton.png
    	deleted:    public/images/gameCards.png
    	deleted:    public/images/gameCards2.png
    	deleted:    public/images/images.jpg
    	deleted:    public/images/login.png
    	deleted:    public/images/logo.png
    	deleted:    public/images/newUser.png
    	modified:   public/js/index.js
    	deleted:    public/js/login.js
    	deleted:    public/styles/layout.css
    	deleted:    public/styles/login.css
    	new file:   public/styles/styles.css
    	modified:   server.js
    	deleted:    views/404.hbs
    	deleted:    views/404.html
    	new file:   views/dashboard.handlebars
    	deleted:    views/games.hbs
    	deleted:    views/index.hbs
    	deleted:    views/index.html
    	modified:   views/layouts/main.handlebars
    	deleted:    views/layouts/main.hbs
    	deleted:    views/login.hbs
    	deleted:    views/login.html
    	deleted:    views/partials/header.hbs
    	deleted:    views/partials/navbar.hbs
    	deleted:    views/partials/scripts.hbs
    	deleted:    views/profilecard.html
    	new file:   views/signin.handlebars
    	new file:   views/signup.handlebars

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var bcrypt = require("bcrypt");
+var passport = require("passport");
+var flash = require("express-flash");
+var session = require("express-session");
 var db = require("./models");
+var methodOverride = require('method-override');
+
+var Users = db.User;
+
+var initializePassport = require("./config/passport-config");
+initializePassport(passport, Users);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -11,6 +20,17 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"))
 
 // Handlebars
 app.engine(
@@ -22,8 +42,82 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// Will load dashboard after user signs in
+app.get("/", checkSignedIn, function(req, res) {
+  //console.log("Made it to dash");
+  //console.log(req.user);
+  res.render("dashboard", { username: req.user.username });
+});
+
+// Renders sign in page
+app.get("/signin", checkNotSignedIn, function(req, res) {
+  res.render("signin");
+});
+
+// Will attempt to authenticate user provided account info with passport-config.js
+app.post(
+  "/signin",
+  checkNotSignedIn,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/signin",
+    failureFlash: true
+  })
+);
+
+// Renders register page
+app.get("/signup", checkNotSignedIn, function(req, res) {
+  res.render("signup");
+});
+
+// Route for registering user
+app.post("/signup", checkNotSignedIn, function(req, res) {
+  console.log("Sign up called");
+  try {
+    console.log(req.body.password);
+    var hashedPassword;
+    // Takes user input password and encrypts it for storage
+    bcrypt.hash(req.body.password, 10).then(function(hash) {
+      // Store hash in your password DB.
+      hashedPassword = hash;
+      console.log(hashedPassword);
+      // Adds user to database
+      Users.create({
+        username: req.body.username,
+        password: hashedPassword,
+        name: req.body.name,
+        email: req.body.email,
+        platform: req.body.platform
+      });
+      // Up completion go to sign in
+      res.redirect("/signin");
+    });
+  } catch (error) {
+    // If error return to signup page
+    res.redirect("/signup");
+  }
+});
+
+//logs the user out and redirects to signin page 
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/signin')
+})
+
+//redirects user if they are not logged in
+function checkSignedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/signin");
+}
+
+function checkNotSignedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
 
 var syncOptions = { force: false };
 
@@ -34,8 +128,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
